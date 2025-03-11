231fd7cfee3e0aae4ac83828a2fa490d7462c26b
Daniel Patrick Studera <145760675+StuderaFHSTP@users.noreply.github.com>
2025-03-11 12:06:44 +0100

    WIP

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { Quiz } from './quiz';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
+  public currentQuiz: Quiz = {
+    id: '1',
+    quizName: 'Test Your Knowledge',
+    questions: [
+    
+    ]
+  }
 
-  constructor() { }
+  constructor() { 
+    this.currentQuiz.questions.push({
+        id: '1',
+        title: 'In welchem Modus ist Mona?',
+        a1: 'normal',
+        a2: 'gechillt',
+        a3: 'BAKAAAAAA',
+        a4: 'wütend',
+        correct: 3
+      });
+  }
 }
